3fb09fd5b3a1fcacf80570ee7601b405a829f0a2
QAQ <115549499+QAQ-0203@users.noreply.github.com>
2024-08-15 01:10:51 +0800

    <fix> Fix openai connection error caused by deprecated model gpt-3.5-turbo-instruct

--- src/search.py
+++ src/search.py
@@ -1,13 +1,13 @@
 import os
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
-from langchain.llms import OpenAI
+#from langchain.llms import OpenAI
 from langchain.agents import AgentType
 import streamlit as st
 
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api"]
 os.environ["SERPAPI_API_KEY"] = st.secrets["Serpapi_API_Key"]
-llm = OpenAI()
+llm = "gpt-4o-mini"
 
 
 def google(question):
